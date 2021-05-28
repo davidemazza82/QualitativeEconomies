7cf3849c5effcb4ce9e6b6d4aed1a6485eba2f30
Davide Mazza <davidemazza82@gmail.com>
2021-05-28 06:45:17 +0000

    PROGRAMPC developped

--- main.py
+++ main.py
@@ -1,6 +1,94 @@
 import numpy as np
 
+# Chapter 4
+def programpc():
+  # Il programma include due loop nested. Il loop esterno fa avanzare il tempo, il loop internofa avanzare I cicli di consume all’interno di un period.
+  TIME = 100 # numero di step temporali
 
+  theta = 0.2 # tax rate
+  alpha1 = 0.6 # alpha1 and alpha2 sono le costanti che determinano il consume formula 3.7
+  alpha2 = 0.4
+  lambda2 = 0.001
+  lambda1 = 0.02
+  lambda0 = 0.2
+
+  N = 100  # numero di iterazioni nel loop interno
+  G = np.zeros(N) # tutte le quantità sono rappresentati con vettori colonna che sono assemblati nella matrice SIM 
+  G[2] = 20 # valore costante del consume del governo
+
+  HINIT = np.zeros(N)
+  HFIN = np.zeros(N)
+  VINIT = np.zeros(N)
+  VFIN = np.zeros(N)
+  BHINIT = np.zeros(N)
+  BHFIN = np.zeros(N)
+  YPERIOD = np.zeros(N)
+  TPERIOD = np.zeros(N)
+  VPERIOD = np.zeros(N)
+  HPERIOD = np.zeros(N)
+  BPERIOD = np.zeros(N)
+  YDPERIOD = np.zeros(N)
+  CPERIOD = np.zeros(N)
+  DHPERIOD = np.zeros(N)
+  DVPERIOD = np.zeros(N)
+  BHPERIOD = np.zeros(N)
+
+  for t in range(0,TIME):
+    if t >= 1: #Il risparmio iniziale ad ogni periodo è uguale al risparmio finale del period precedente. Il primo deve essere forzato a zero per cui il ciclo if
+        VINIT[t] = VFIN[t-1]
+        HINIT[t] = HFIN[t-1]
+        BHINIT[t] = BHFIN[t-1]
+    elif t == 0:
+        VINIT[1] = 0
+        HINIT[1] = 0
+        BHINIT[1] = 0
+
+    r = 0.02 * np.ones(N) # vettore tassi di interesse
+    Y = np.zeros(N) # vettore influssi di cassa per ogni household
+    YD = np.zeros(N) # vettore del disposable income
+    C = np.zeros(N) # vettore consume delle household; ATTN. ogni element del vettore è il consume in ogni ciclo interno non quello globale del periodo
+    T = np.zeros(N) # vettore tasse ad ogni ciclo
+    B = np.zeros(N) # bills posseduti dalle households
+    BH = np.zeros(N) # bills posseduti dalle households
+    DH = np.zeros(N) # vettore incrememnto risparmio ad ogni ciclo
+    DV = np.zeros(N) # vettore incrememnto risparmio ad ogni ciclo
+    DBH = np.zeros(N) # vettore incrememnto risparmio ad ogni ciclo
+    V = np.zeros(N) # vettore incrememnto risparmio ad ogni ciclo
+    V[1] = VINIT[t] # valore del risparmio iniziale ad ogni periodo; tutti gli altri elementi di V sono uguali a zero
+    # inizia il loop interno per ogni time step; ad ogni valore di I corrisponde un ciclo complete, ricezione dell’ordine da parte delle aziende, pagamento dei dipendenti, pagamento delle tasse, consume e risparmio aggiornamento del risparmio
+    for i in range(1,N):
+      Y[i] = G[i] + C[i-1] # Y è il totale della transazione 
+      T[i] = theta * ( Y[i] + r[i-1] * BH[i-1] ) # pagamentodelle tasse
+      YD[i] = Y[i] - T[i] + r[i-1] * BH[i-1] # Disposable income = pagamenti ricevuti meno tasse pagate
+      C[i] = alpha1 * YD[i] + alpha2 * V[i-1] # Determinazione del consume al prossimo periodo. È un numero sempre inferior a TD(i)
+      DV[i] = YD[i] - C[i]
+      DH[i] = DV[i] * ( 1 - lambda0 ) - lambda1 * r[i] + lambda2 * YD[i]
+      DBH[i] = DV[i] * lambda0 + lambda1 * r[i] - lambda2 * YD[i]
+              
+    VFIN[t] = VINIT[t] + sum(DV)
+    VPERIOD[t] = VFIN[t]
+    HFIN[t] = HINIT[t] + sum(DH)
+    HPERIOD[t] = HFIN[t]
+    BHFIN[t] = BHINIT[t] + sum(DBH)
+    BHPERIOD[t] = BHFIN[t]
+    
+    YPERIOD[t] = sum(Y)
+    TPERIOD[t] = sum(T)
+    YDPERIOD[t] = sum(YD)
+    CPERIOD[t] = sum(C)
+    DVPERIOD[t] = sum(DV)
+    BPERIOD[t] = B[N-1]
+
+  RES = [YPERIOD, TPERIOD, YDPERIOD, VPERIOD, CPERIOD,  HPERIOD, BPERIOD]
+
+  print(YPERIOD[1], YPERIOD[2], YPERIOD[TIME-1])
+  print(TPERIOD[1], TPERIOD[2], TPERIOD[TIME-1])
+  print(YDPERIOD[1], YDPERIOD[2],	YDPERIOD[TIME-1])
+  print(CPERIOD[1],	CPERIOD[2],	CPERIOD[TIME-1])
+  print(DHPERIOD[1], DHPERIOD[2], DHPERIOD[TIME-1])
+
+
+# Chapter 3
 def simex():
   N = 100 # numero di step temporali
 
@@ -20,6 +108,7 @@ def simex():
   H = np.zeros(N) # vettore ricchezza
   DH = np.zeros(N) # vettore incrememnto ricchezza ad ogni ciclo
 
+  print("Ciao")
   for t in range(1,N):
     if t >= 3: # Il risparmio iniziale ad ogni periodo è uguale al risparmio finale del period precedente. Il primo deve essere forzato a zero per cui il ciclo if
       YDE[t] = YD[t-1]
@@ -37,11 +126,11 @@ def simex():
     H[t] = H[t-1] + DH[t] # risparmio alla fine del period t
     
     SIM=[G, Y, T, C, YDE, DH, H] # matrice con tutti I termini precedent. Una riga è un ciclo i
+ 
+  print(SIM[1])
+  
 
-  print(SIM[1], SIM[2])
-
-
-
+# Chapter 3
 def sim():
   # Il programma include due loop nested. Il loop esterno fa avanzare il tempo, il loop internofa avanzare I cicli di consume all’interno di un period.
   T = 1000 # numero di step temporali
@@ -103,4 +192,4 @@ def sim():
   print(DHPERIOD[1], DHPERIOD[2], DHPERIOD[T-1])
 
 
-simex()
+programpc()
